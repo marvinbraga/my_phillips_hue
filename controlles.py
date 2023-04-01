53aadae019f3de903ec552d3483a381e4efe423f
Marcus Braga <mvbraga@gmail.com>
2023-04-01 16:43:55 -0300

    Adjusts and new setups.

--- controlles.py
+++ controlles.py
@@ -11,16 +11,20 @@ class HueController:
         self.bridge.connect()
         self.lights = self.bridge.get_light_objects()
 
-    def set_light_color(self, light_name, color: Color):
+    def set_light_color(self, light_name, color: Color) -> Light:
         xy = RGBtoXYAdapter.convert(color.red, color.green, color.blue)
         light = self._get_light_by_name(light_name)
-        if light:
-            light.xy = xy
-            light.brightness = color.brightness
+        light.xy = xy
+        light.brightness = color.brightness
+        return light
 
-    def apply_light_config(self, light_config: LightConfig):
+    def apply_light_config(self, light_config: LightConfig, transition_time_secs=0):
         for setting in light_config.settings:
-            self.set_light_color(setting.light_name, setting.color)
+            light = self.set_light_color(setting.light_name, setting.color)
+            if transition_time_secs:
+                light.transitiontime = transition_time_secs * 10
+                light.on = True
+        return self
 
     def _get_light_by_name(self, light_name):
         for light in self.lights:
--- main.py
+++ main.py
@@ -4,13 +4,13 @@ from decouple import config
 from controlles import HueController
 from factories import LightConfigEnum
 
-hue = HueController(config("bridge_ip"))
+hue = HueController(ip_address=config("bridge_ip"))
 
 
 def test(config_enum):
     setup = config_enum.get_instance()
     print(f"Name: {setup}, Description: {config_enum.description}")
-    hue.apply_light_config(setup)
+    hue.apply_light_config(setup, transition_time_secs=3)
 
 
 def test_all():
@@ -20,5 +20,5 @@ def test_all():
 
 
 if __name__ == '__main__':
-    test(LightConfigEnum.SETUP_RELAXING)
-    # test_all()
+    # test(LightConfigEnum.SETUP_RELAXING)
+    test_all()
